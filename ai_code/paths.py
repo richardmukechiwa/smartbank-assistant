8e7d2a044c591e3e37e2dfe913b8bc408dfbcd62
Richard <mukechiwarichard@gmail.com>
2025-06-20 22:36:19 +0200

    rag setup successful

--- ai_code/paths.py
+++ ai_code/paths.py
@@ -5,7 +5,7 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ENV_FPATH = os.path.join(ROOT_DIR, ".env")
 
-CODE_DIR = os.path.join(ROOT_DIR, "code")
+CODE_DIR = os.path.join(ROOT_DIR, "ai_code")
 
 APP_CONFIG_FPATH = os.path.join(CODE_DIR, "config", "config.yaml")
 PROMPT_CONFIG_FPATH = os.path.join(CODE_DIR, "config", "prompt_config.yaml")
@@ -15,7 +15,7 @@ OUTPUTS_DIR = os.path.join(ROOT_DIR, "outputs")
 
 
 DATA_DIR = os.path.join(ROOT_DIR, "data")
-PUBLICATION_FPATH = os.path.join(DATA_DIR, "publication.md")
+PUBLICATION_FPATH = os.path.join(DATA_DIR, "electronic_payments_full_guide.md")
 
 VECTOR_DB_DIR = os.path.join(OUTPUTS_DIR, "vector_db")
 
